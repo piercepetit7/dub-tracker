315ddfff25d0e1b1b27970f52cbfe2622b83583a
Pierce Petit <ppetit2496@gmail.com>
2022-06-25 12:35:39 -0500

    finish set up commit

--- server.js
+++ server.js
@@ -18,7 +18,6 @@ import('./config/passport.js')
 // require routes
 import { router as indexRouter } from './routes/index.js'
 import { router as authRouter } from './routes/auth.js'
-import { router as winsRouter } from './routes/wins.js'
 
 
 // create the express app
@@ -64,7 +63,7 @@ app.use(passUserToView)
 // router middleware
 app.use('/', indexRouter)
 app.use('/auth', authRouter)
-app.use('/wins', winsRouter)
+
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404))
