8a642b1197742a239c559f856e7c585300cbb549
Pierce Petit <ppetit2496@gmail.com>
2022-06-29 14:43:35 -0500

    show wins on profile commit

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -1,4 +1,5 @@
 import { Profile } from "../models/profile.js"
+import { Win } from "../models/win.js"
 
 function index(req, res) {
     Profile.find({})
@@ -13,15 +14,20 @@ function index(req, res) {
 function show(req, res) {
     Profile.findById(req.params.id)
     .then((profile) => {
-        Profile.findById(req.user.profile)
-        .then(userProfile => {
-            res.render("profiles/show", {
-                title: `${profile.name}'s profile`,
-                profile,
-                userProfile,
+        Win.find({ owner: profile._id })
+        .populate("owner")
+            .then((wins) => {
+                Profile.findById(req.user.profile)
+                .then(userProfile => {
+                    res.render("profiles/show", {
+                        title: `${profile.name}'s profile`,
+                        profile,
+                        userProfile,
+                        wins,
+                    })
+                })
             })
         })
-    })
     .catch(err => {
         console.log(err)
         res.redirect("/")
