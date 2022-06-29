44dcb74225305559b1504fb21961a535d25b2d21
Pierce Petit <ppetit2496@gmail.com>
2022-06-29 18:59:11 -0500

    and ninja emoji commit

--- models/comment.js
+++ models/comment.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 const Schema = mongoose.Schema;
 
 const commentSchema = new Schema({
-    rating: {type: Number, min: 1, max: 5},
+    rating: {type: Number, min: 0, max: 5},
     content: String,
     author: { type: Schema.Types.ObjectId, ref: "Profile" },
     win: { type: Schema.Types.ObjectId, ref: "Win" }
